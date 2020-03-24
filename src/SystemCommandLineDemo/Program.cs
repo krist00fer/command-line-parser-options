ab0ffcdef769de3e230c962ba69f26bd49428019
Kristofer Liljeblad <kristofer.liljeblad@live.com>
2020-03-24 22:01:27 +0100

    minor adjustments

--- src/SystemCommandLineDemo/Program.cs
+++ src/SystemCommandLineDemo/Program.cs
@@ -7,10 +7,14 @@ namespace SystemCommandLineDemo
         // This example uses the System.CommandLine and System.ComandLine.DragonFruit packages
         // https://github.com/dotnet/command-line-api
         
+        /// <summary>
+        /// Sample application, parsing arguments using DragonFruit
+        /// </summary>
         /// <param name="useTls">Starts server using Transport Layer Security, TLS</param>
-        static void Main(bool useTls)
+        /// <param name="port">Set the port for the server to listen to, default = 80</param>
+        static void Main(bool useTls, int port = 80)
         {
-            Console.WriteLine($"Use TLS flagg was set to: {useTls}");
+            Console.WriteLine($"Use TLS flagg was set to: {useTls} and port: {port} {Boolean.FalseString}");
         }
     }
 }
